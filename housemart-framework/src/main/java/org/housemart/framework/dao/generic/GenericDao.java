dbc3471a2bc6a650e08cac579b9001010b7d62b9
hapereklu <haperkelu@gmail.com>
2014-01-07 16:50:47 +0800

    Adjust framwork(or mapping)

--- housemart-framework/src/main/java/org/housemart/framework/dao/generic/GenericDao.java
+++ housemart-framework/src/main/java/org/housemart/framework/dao/generic/GenericDao.java
@@ -1,6 +1,8 @@
 package org.housemart.framework.dao.generic;
 
 import java.util.List;
+import java.util.Map;
+import org.housemart.framework.dao.generic.PaginateObject;
 
 public interface GenericDao<T> {	
 	
@@ -9,12 +11,17 @@ public interface GenericDao<T> {
 	int add(String statementid, Object para);
 	
 	T load(String statementid, int id);
+
+	@SuppressWarnings("rawtypes")
+	T load(String statementid, Map para);
 	
 	int delete(String statementid, int id);
 	
 	int delete(String statementid, Object obj);
 	
 	int deletePhysical(String statementid, int id);
+
+	int deletePhysical(String statementid, Object args);
 	
 	int update(String statementid, Object para);
 	
@@ -22,6 +29,8 @@ public interface GenericDao<T> {
 	
 	<S> List<S> selectByType(String statementid, Object args);
 	
+	PaginateObject paginate(String statementid, int pageNo, int pageSize,
+			Object para);
 }
 
 
--- housemart-framework/src/main/java/org/housemart/framework/dao/generic/GenericDaoImp.java
+++ housemart-framework/src/main/java/org/housemart/framework/dao/generic/GenericDaoImp.java
@@ -1,6 +1,9 @@
 package org.housemart.framework.dao.generic;
 
 import java.util.List;
+import java.util.Map;
+import org.apache.commons.collections.CollectionUtils;
+import org.housemart.framework.dao.generic.PaginateObject;
 import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
 
 
@@ -74,6 +77,43 @@ public class GenericDaoImp<T> extends SqlMapClientDaoSupport implements GenericD
 
 	public int delete(String statementid, Object obj) {
 		return getSqlMapClientTemplate().delete(getNamespaceStatement(statementid), obj);
+	}
+
+	@Override
+	public T load(String statementid, Map para) {
+		T result = (T) getSqlMapClientTemplate().queryForObject(getNamespaceStatement(statementid), para);		
+		return result;
+	}
+
+	@Override
+	public int deletePhysical(String statementid, Object args) {
+		return getSqlMapClientTemplate().delete(getNamespaceStatement(statementid), args);
+	}
+
+	@Override
+	public PaginateObject paginate(String statementid, int pageNo,
+			int pageSize, Object para) {
+		PaginateObject obj = new PaginateObject();
+		List<Object> result = null;
+		
+		if(para != null && para instanceof Map){
+			((Map)para).put("skip", 0);
+			((Map)para).put("count", Integer.MAX_VALUE);
+		}
+		result = getSqlMapClientTemplate().queryForList(getNamespaceStatement(statementid), para);
+		int count = CollectionUtils.isEmpty(result) ? 0: result.size();
+		
+		if(para != null && para instanceof Map){
+			((Map)para).put("skip", pageNo == 0 ? 0: pageNo * pageSize);
+			((Map)para).put("count", pageSize);
+		}
+		result = getSqlMapClientTemplate().queryForList(getNamespaceStatement(statementid), para);
+		obj.setResult(result);
+		obj.setPageNo(pageNo);
+		obj.setPageSize(pageSize);
+		obj.setCount(count);
+		
+		return obj;
 	}
 
 
